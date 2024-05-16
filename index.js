c00dbe6ab9bbc41098746f3383129e73e7f108f4
David Chatting <david@davidchatting.com>
2024-05-16 21:35:05 +0100

    basic express server with static page servering and stress endpoints

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const app = express()
+const cp = require('child_process')
+const port = 8080
+
+app.use(express.static('public_html'))
+
+app.get('/stress', (req, res) => {
+cp.exec('stress -c 4 -t 20s', (err, stdout, sterr) => {
+if(err) {
+console.log(err)
+res.sendStatus(500)
+return
+}
+console.log(stdout)
+res.json({message:stdout})
+})
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+
