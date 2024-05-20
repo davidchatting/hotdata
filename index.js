74675b1d3d6f515f0200d67a1b4d8d5077b7a076
David Chatting <david@davidchatting.com>
2024-05-20 10:45:12 +0100

    Update index.js
    Add /tick endpoint to return processor temperature

--- index.js
+++ index.js
@@ -6,15 +6,27 @@ const port = 8080
 app.use(express.static('public_html'))
 
 app.get('/stress', (req, res) => {
-cp.exec('stress -c 4 -t 20s', (err, stdout, sterr) => {
-if(err) {
-console.log(err)
-res.sendStatus(500)
-return
-}
-console.log(stdout)
-res.json({message:stdout})
+  cp.exec('stress -c 4 -t 20s', (err, stdout, sterr) => {
+    if(err) {
+      console.log(err)
+      res.sendStatus(500)
+      return
+    }
+    console.log(stdout)
+    res.json({message:stdout})
+  })
 })
+
+app.get('/tick', (req, res) => {
+  cp.exec('vcgencmd measure_temp', (err, stdout, sterr) => {
+    if(err) {
+      console.log(err)
+      res.sendStatus(500)
+      return
+    }
+    console.log(stdout)
+    res.json({message:stdout})
+  })
 })
 
 app.listen(port, () => {
