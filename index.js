50850399c26f7d7941b6cc04fc18a6d848f85d3d
David Chatting <david@davidchatting.com>
2024-05-27 13:11:01 +0100

    parameterise /stress + parse temperature as float

--- index.js
+++ index.js
@@ -6,27 +6,32 @@ const port = 8080
 app.use(express.static('public_html'))
 
 app.get('/yes', (req, res) => {
-  cp.exec('stress -c 4 -t 20s', (err, stdout, sterr) => {
-    if(err) {
+  let timeSec = 10;
+  if(req.query.t) timeSec = Number.parseInt(req.query.t)
+  
+  let command = 'stress -c 4 -t ' + timeSec + 's'
+  cp.exec(command, (err, stdout, sterr) => {
+    if(!err) {
+      res.sendStatus(200)
+    }
+    else {
       console.log(err)
       res.sendStatus(500)
-      return
     }
-    console.log(stdout)
-    res.json({message:stdout})
   })
 })
 
 app.get('/tick', (req, res) => {
-  let result = {tock:true};
+  let result = {tock:true}
 
   cp.exec('vcgencmd measure_temp', (err, stdout, sterr) => {
     if(!err) {
       let s = stdout.trim().split('=')
       if(s.length == 2) {
-        result[s[0]] = s[1]
+        result[s[0]] = Number.parseFloat(s[1])
       }
     }
+    console.log(result)
     res.json(result)
   })
 })
